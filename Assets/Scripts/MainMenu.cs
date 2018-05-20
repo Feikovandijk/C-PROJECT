1de8d1fa500f76cda0f73706a33d1a2bc61f3ee4
Feiko van Dijk <feiko60@gmail.com>
2018-05-20 23:14:05 +0200

    [ADDED] Main menu [CHANGED] Auto generating lighting
    Main menu is now added in main_menu, added 2 buttons. Also fixed a lighting issue by disabling the automatic generation of lighting.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 45), "Play"))
+        {
+            Application.LoadLevel(1);
+        }
+
+        if (GUI.Button(new Rect(10, 65, 100, 45), "Quit"))
+        {
+            Application.Quit();
+        }
+    }
+    
+
+}
